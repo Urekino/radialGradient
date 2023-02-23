1915e235bb5c2f51a53caef64cc9cf6e67054430
Argiris Kan <44966876+Urekino@users.noreply.github.com>
2023-02-24 00:09:17 +0200

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,9 @@
+
+let bor = document.querySelector('.borderId');
+bor.addEventListener('mousemove', e => {
+    let rect = e.target.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+    bor.style.setProperty('--x', x + 'px');
+    bor.style.setProperty('--y', y + 'px');
+});
